51bf3fd362723b6f4aa719e44bfc24b553028148
glaucia86 <glaucia_lemos86@hotmail.com>
2019-10-15 14:12:34 -0700

    Criando a estrutura inicial do projeto e starter inicial do projeto.

---
+++ src/face-detection.js
@@ -0,0 +1,7 @@
+/**
+ * arquivo: src/facedetection.js
+ * Data: 16/10/019
+ * Descrição: Arquivo responsável por realizar o reconhecimento facial usando o Face API & Node.js
+ * Author: Glaucia Lemos
+ */
+
