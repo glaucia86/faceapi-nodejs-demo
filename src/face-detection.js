357525043afc7739066420f67f917e2d84ad93b4
glaucia86 <glaucia_lemos86@hotmail.com>
2019-10-16 14:23:01 -0700

    Início do desenvolvimento da lógica do arquivo 'face-detection.js'

--- src/face-detection.js
+++ src/face-detection.js
@@ -5,3 +5,12 @@
  * Author: Glaucia Lemos
  */
 
+'use strict';
+
+const request = require('request');
+
+require('dotenv').config();
+
+const subscriptionKey = process.env.SUBSCRIPTION_FACE_API_KEY;
+const uriBase = process.env.URI_BASE;
+const imageUrl = 'https://cdn-ofuxico.akamaized.net/img/upload/noticias/2019/05/13/silvio_santos_reproducao_instagram_349201_36.jpg';
